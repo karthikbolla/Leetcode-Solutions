b0822e72f7400e6b154e77fca40625a6f87ff9dc
KARTHIK TARAKA SAI BOLLA <110156560+karthikbolla@users.noreply.github.com>
2023-05-23 17:03:28 +0530

    Time: 0 ms (100.00%), Space: 54.7 MB (99.02%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s=0;
+        int e=arr.length-1;
+        while(s!=e){
+            int m =s+(e-s)/2;
+            if(arr[m]<arr[m+1]){
+                s=m+1;
+            }
+            else if(arr[m]>arr[m+1]){
+                e=m;
+            }
+        }
+       return s;
+    }
+}
