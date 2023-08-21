06ba44f6c39d7f3e8417eca40f30a203d8c17afe
KARTHIK TARAKA SAI BOLLA <110156560+karthikbolla@users.noreply.github.com>
2023-08-21 20:22:16 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (90.78%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -25,15 +25,11 @@ class Solution {
                 tail=tail.next;
             }
         }
-        while(list1!=null){
+        if(list1!=null){
             tail.next=list1;
-                list1=list1.next;
-                tail=tail.next;
         }
-        while(list2!=null){
+        if(list2!=null){
              tail.next=list2;
-                list2=list2.next;
-                tail=tail.next;
         }
         return head.next;
     }
